fb96286ea5416d00807dbe1d09fae57e74fe6b9b
Developer <Developer>
2016-04-01 17:01:42 +0300

    menu

--- bitrix/templates/main/js/app.js
+++ bitrix/templates/main/js/app.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-	// body...
+	function burger(){
+		var burger = $('.humburger'),
+				nav = $('.navigation'),
+				close = nav.find('.close');
+
+		burger.on('click', function(){
+			nav.addClass('inner-nav')
+		});
+		close.on('click', function(){
+			nav.removeClass('inner-nav')
+		});
+	};
+	burger();
 })
